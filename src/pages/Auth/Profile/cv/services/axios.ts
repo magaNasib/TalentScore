ffbf72d30256e770cf996ea8bf59be6ff3fadd76
magaNasib <mehemmednessibov3@gmail.com>
2023-11-01 11:03:13 +0400

    last

--- src/pages/Auth/Profile/cv/services/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-});
-
-export const read = async (path: string) => (await instance.get(path)).data;
